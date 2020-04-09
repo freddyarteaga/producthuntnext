fc65cc177134c59215775970520f3915eb94fc89
Freddy <freddy.artea@gmail.com>
2020-04-09 12:57:38 -0500

    adding elements to the header

---
+++ components/layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Search from '../ui/Search'
+
+const Header = () => {
+    return ( 
+        <header>
+            <div>
+                <div>
+                    <p>P</p>
+
+                    <Search />
+
+                    {/* Nav Here */}
+                </div>
+                <div>
+                    {/* Admin Menu */}
+                </div>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Link from 'next/link'
+import Header from './Header';
 
 const Layout = props => {
     return ( 
         <>
-            <h1>Header</h1>
-
-            <nav>
-                <Link href='/'>Init</Link>
-                <Link href='/we'>We</Link>
-            </nav>
+            <Header />
 
             <main>
                 {props.children}
