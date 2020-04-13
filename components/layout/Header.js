59d8e0f066ab28bd7fc034e8a5d4a89cda651911
Freddy <freddy.artea@gmail.com>
2020-04-13 09:37:03 -0500

    css to header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,13 +2,41 @@ import React from 'react';
 import Search from '../ui/Search';
 import Navigation from './Navigation';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const ContainHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`
 
 const Header = () => {
     return ( 
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <ContainHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Search />
 
@@ -22,7 +50,7 @@ const Header = () => {
                     <Link href="">Login</Link>
                     <Link href="">Create Account</Link>
                 </div>
-            </div>
+            </ContainHeader>
         </header>
      );
 }
