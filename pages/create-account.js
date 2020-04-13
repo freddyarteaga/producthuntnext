fd7673f169166fd35d1a239727ec11f3a9d510ea
Freddy <freddy.artea@gmail.com>
2020-04-13 11:00:31 -0500

    creating the form to create account

--- pages/create-account.js
+++ pages/create-account.js
@@ -4,7 +4,25 @@ import Layout from '../components/layout/Layout'
 const CreateAccount = () => (
   <div>
     <Layout>
-      <h1>Create Account</h1>
+        <>
+            <h1>Create Account</h1>
+            <form>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" placeholder="you Name" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="you Email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">password</label>
+                    <input type="password" id="password" placeholder="you Password" name="password" />
+                </div>
+
+                <input type="submit" value="Create Account" />
+            </form>
+        </>
     </Layout>
   </div>
 )
