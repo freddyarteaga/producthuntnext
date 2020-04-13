34f1bccece774a930fe9faecfd6a88a973770c94
Freddy <freddy.artea@gmail.com>
2020-04-13 11:13:21 -0500

    css for forms

--- pages/create-account.js
+++ pages/create-account.js
@@ -1,27 +1,34 @@
 import React from 'react'
+import { css } from '@emotion/core';
 import Layout from '../components/layout/Layout'
+import { Form, TextBox, InputSubmit } from '../components/ui/Form';
 
 const CreateAccount = () => (
   <div>
     <Layout>
         <>
-            <h1>Create Account</h1>
-            <form>
-                <div>
+            <h1
+                css={css`
+                    text-align: center;
+                    margin-top: 5rem;
+                `}
+            >Create Account</h1>
+            <Form>
+                <TextBox>
                     <label htmlFor="name">Name</label>
                     <input type="text" id="name" placeholder="you Name" name="name" />
-                </div>
-                <div>
+                </TextBox>
+                <TextBox>
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" placeholder="you Email" name="email" />
-                </div>
-                <div>
+                </TextBox>
+                <TextBox>
                     <label htmlFor="password">password</label>
                     <input type="password" id="password" placeholder="you Password" name="password" />
-                </div>
+                </TextBox>
 
-                <input type="submit" value="Create Account" />
-            </form>
+                <InputSubmit type="submit" value="Create Account" />
+            </Form>
         </>
     </Layout>
   </div>
